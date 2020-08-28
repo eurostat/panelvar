934b9947d55b7199158e5d3f7820431627341b24
Jacopo Grazzini <gjacopo@gmail.com>
2020-08-28 15:23:03 +0200

    udpated path

--- bin/pvar2.sh
+++ bin/pvar2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nohup R CMD BATCH ./src/PVAR-m1.R ./src/PVAR-m1.Rout &
-nohup R CMD BATCH ./src/PVAR-m2.R ./src/PVAR-m2.Rout &
-nohup R CMD BATCH ./src/PVAR-m3.R ./src/PVAR-m3.Rout &
-nohup R CMD BATCH ./src/PVAR-q1.R ./src/PVAR-q1.Rout &
+nohup R CMD BATCH ../src/PVAR-m1.R ../src/PVAR-m1.Rout &
+nohup R CMD BATCH ../src/PVAR-m2.R ../src/PVAR-m2.Rout &
+nohup R CMD BATCH ../src/PVAR-m3.R ../src/PVAR-m3.Rout &
+nohup R CMD BATCH ../src/PVAR-q1.R ../src/PVAR-q1.Rout &
 
